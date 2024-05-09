9c415dc9b98c172fec731d336935e45da1ecee0b
SOPHIA OLIVEIRA ACUNZO <40173049826@info.local>
2024-05-09 09:42:23 -0300

    feat: exercico2

---
+++ ex2.js
@@ -0,0 +1,22 @@
+// Exercício 2: Verificação de dia da semana
+// Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da
+// semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira,
+// 2 representa terça-feira e assim por diante.
+
+// Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da semana
+
+let dia = prompt("digite um numero de 1 a 7: ") 
+
+if (dia == 1)  {console.log("hoje e segunda feira")}
+
+if (dia == 2)  {console.log("hoje e terca feira")}
+
+if (dia == 3)  {console.log("hoje e quarta feira")}
+
+if (dia ==4)  {console.log("hoje e quinta feira")}
+
+if (dia == 5)  {console.log("hoje e sexta feira")}
+
+if (dia == 6) {console.log("hoje e sabado")}
+
+if (dia == 7)  {console.log("domigo")}
