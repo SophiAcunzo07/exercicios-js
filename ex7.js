a4f59ecbffbd02fb18817757b3354ea34a7c5dab
SOPHIA OLIVEIRA ACUNZO <40173049826@info.local>
2024-05-10 08:45:51 -0300

    feat: exercicio7

---
+++ ex7.js
@@ -0,0 +1,42 @@
+// Exercício 7: Cálculo de desconto progressivo
+
+// Imagine uma loja de roupas que dê um desconto ao cliente, dependendo do valor que ele
+// pague. Faça um programa para loja, que solicite o valor total da compra ao usuário e assim,
+// aplique um desconto de acordo com o valor total da compra. Siga a tabela:
+// Valor total da compra até R$ 100,00: sem desconto.
+// Valor total da compra de R$ 100,01 a R$ 200,00: desconto de 10%.
+// Valor total da compra acima de R$ 200,00: desconto de 20%.
+// Exiba o valor final da compra com o desconto aplicado.
+
+let valorCompra = prompt("Digite o valor total da compra:");
+
+// Converte o valor da compra para um número
+
+valorCompra = parseFloat(valorCompra);
+
+// Define variável para armazenar o valor final da compra com desconto
+
+let valorFinal;
+
+// Verifica o valor da compra e aplica o desconto progressivo
+
+if (valorCompra <= 100) {
+
+    // Sem desconto
+
+    valorFinal = valorCompra;
+} else if (valorCompra <= 200) {
+
+    // Desconto de 10%
+
+    valorFinal = valorCompra * 0.9;
+} else {
+
+    // Desconto de 20%
+
+    valorFinal = valorCompra * 0.8;
+}
+
+// Exibe o valor final da compra com desconto aplicado
+
+console.log("O valor final da compra com desconto é: R$" + valorFinal.toFixed(2));
