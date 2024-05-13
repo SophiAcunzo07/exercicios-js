dca6423a82ae6c6ecda0f5c925c78e77ec4d253d
SOPHIA OLIVEIRA ACUNZO <40173049826@info.local>
2024-05-13 10:48:21 -0300

    feat: exercicio 1

---
+++ loop/exemplos2.js
@@ -0,0 +1,47 @@
+// for - loop contato
+
+//contanto de 0 a 10
+// for (let num = 0; num <= 10; num++ ) {
+//     console.log(num)
+// }
+
+// //contagem regressiva
+// for (let cont = 10; cont >= 0; cont--) {
+//     console.log(cont)
+// } 
+
+//while- loop condicional
+
+// // let salario = 1000
+
+// while( salario < 5000) {
+//     //salario = salario + 100;
+//     salario += 100;
+
+//     console.log("o salario ainda e R$ " + salario)
+// }
+
+// // loop condicional - while do
+
+// let saldo = 1000
+
+// do { 
+//     console.log("esse e seu saldo: " + saldo);
+//     saldo = 100
+// } while (saldo<5000);
+
+// Array
+
+let frutas =  ["banna", "laranja", "maca", "pitaya", "melancia"] 
+
+// console.log(frutas[3])
+
+// frutas.forEach(fruta => {
+//     console.log("a fruta e: " + fruta)
+// }
+//     );
+
+//percorrendo uma lista
+for(let indicie = 0; indice < 5; indicie ++){
+    console.log(frutas[indicie])
+}
