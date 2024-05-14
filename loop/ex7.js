d356d38d1d9cb4e3316ba28142b1632c262eb2b1
SOPHIA OLIVEIRA ACUNZO <40173049826@info.local>
2024-05-14 08:25:35 -0300

    feat: exercicio 7

---
+++ loop/ex7.js
@@ -0,0 +1,12 @@
+// Calcular a soma dos números de 1 a 100 utilizando a estrutura
+// while.
+
+let numero = 1;
+let soma = 0;
+
+while (numero <= 100) {
+    soma += numero;
+    numero++;
+}
+
+console.log("a soma dos numeros del 1 a 100 sao:", soma);
